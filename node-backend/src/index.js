fe21dbc11c30579af41052bd90b635926f684459
Ahmad Saad <kasaad1609@gmail.com>
2025-05-30 17:15:18 +0530

    node-backend on render

--- node-backend/src/index.js
+++ node-backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 
@@ -9,5 +9,5 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/flashcards", flashcardRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
